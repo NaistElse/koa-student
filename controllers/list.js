a4e4945ff9bac3c3e91a3a26be8173b1c88ce985
applestore <apple@applestoredeMacBook-Pro.local>
2020-10-03 23:16:32 +0800

    添加分页功能

--- controllers/list.js
+++ controllers/list.js
@@ -1,24 +1,10 @@
 // 数据列表主页
 const db = require('../database')
 
-// var students = [
-//   {
-//     'sno': '02222',
-//     'sname': '张三'
-//   },
-//   {
-//     'sno': '02223',
-//     'sname': '张三3'
-//   },
-//   {
-//     'sno': '02224',
-//     'sname': '张三4'
-//   },
-//   {
-//     'sno': '02225',
-//     'sname': '张三4'
-//   }
-// ]
+
+const everypage_size = 5 // 每页所展示的学生数
+var total_page = 0
+
 
 module.exports = {
   'GET /list': async(ctx, next) => {
@@ -27,23 +13,76 @@ module.exports = {
     }
 
     var
-        sname = ctx.query.sname || ''
+        sname = ctx.query.sname || '',
+        page = ctx.query.page ? parseInt(ctx.query.page) : 1
+
+
+    page = page < 1 ? 1 : page
+
+    console.log(page)
 
     const specialtycategory = await db.query('select * from specialty')
 
+    // 总的学生
+    const total_students = await db.query(`SELECT COUNT(1) AS count FROM students INNER JOIN specialty ON students.sdept_id = specialty.id`)
+
+
+    total_pages = parseInt(Math.ceil(total_students[0].count / everypage_size)) // 总的页数
+
+    page = page > total_pages ? total_pages : page
+
+    var offset = (page - 1) * everypage_size
+
+
     if(sname) {
-      const findstudent = await db.query(`SELECT students.sno, students.sname, students.ssex, students.sclass, students.spwd, students.sdept_id, specialty.category FROM students INNER JOIN specialty ON students.sdept_id = specialty.id where sname =  '${sname}'`)    
+      const findstudent = await db.query(`SELECT students.sno, students.sname,
+                                          students.ssex, students.sclass, students.spwd,
+                                          students.sdept_id, specialty.category FROM students
+                                          INNER JOIN specialty ON students.sdept_id = specialty.id
+                                          where sname =  '${sname}'`)
+
       return ctx.render('list', {
         students: findstudent,
         specialtycategory,
-        stauts: findstudent == '' ? 1 : 0
+        stauts: findstudent == '' ? 1 : 0,
+        pagination: 0
       })
     }
 
-    const students = await db.query('SELECT students.sno, students.sname, students.ssex, students.sclass, students.spwd, students.sdept_id, specialty.category FROM students INNER JOIN specialty ON students.sdept_id = specialty.id')
+    const students = await db.query(`SELECT students.sno, students.sname,
+                                     students.ssex, students.sclass, students.spwd,
+                                     students.sdept_id, specialty.category FROM students
+                                     INNER JOIN specialty ON students.sdept_id = specialty.id
+                                     LIMIT ${offset}, ${everypage_size}`)
+
+
+     // 分页处理逻辑
+     var visiable = 5
+
+     var begin = page - (visiable -1 ) / 2
+     var end = begin + visiable - 1
+
+     begin = begin < 1 ? 1 : begin
+     end = begin + visiable - 1
+     end = end > total_pages ? total_pages : end
+     begin = end - visiable + 1
+     begin = begin < 1 ? 1 : begin
+
+
+     var pagination = {
+        begin,
+        page,
+        end,
+        total_pages
+    }
+
+    console.log(pagination)
+
+
     await ctx.render('list', {
       students,
-      specialtycategory
+      specialtycategory,
+      pagination
     })
 
   }
